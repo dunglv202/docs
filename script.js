e01e56819757fdf4db0a1bc74f83fe8b57861cfc
vandung2kk2 <vandung2kk2@gmail.com>
2022-01-26 18:25:16 +0700

    sf

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+const mainSec = document.getElementById("main");
+const createList = function(data){
+    for (let i=0; i<data.length; i++) {
+        let newSec = document.createElement("section");
+        newSec.classList.add("sec");
+        newSec.innerHTML = `
+        
+        `
+    }
+}
+
 let request = new XMLHttpRequest();
 request.open("GET", "https://docs.dunglv202.tk/db.json");
 request.onreadystatechange  = function(){
     if(this.readyState == 4){
-        console.log(this.responseText);
+        console.log();
+        createList(JSON.parse(this.responseText).data);
     }
 }
 request.send();

49260a5260a1a728b82fda1b16586110f1f48412
vandung2kk2 <vandung2kk2@gmail.com>
2022-01-26 19:15:18 +0700

    hs

--- script.js
+++ script.js
@@ -4,8 +4,31 @@ const createList = function(data){
         let newSec = document.createElement("section");
         newSec.classList.add("sec");
         newSec.innerHTML = `
-        
+            <h1 class="sec-heading">${data[i].heading}</h1>
         `
+        // create div grid-container and ul no-style list
+        let container = document.createElement("div");
+        container.classList.add("grid-container");
+
+        let list = document.createElement("ul");
+        list.classList.add("no-style", "list");
+
+        // create list items
+        let items = data[i].list;
+        for (let j=0; j<items.length; j++) {
+            let listItem = document.createElement("li");
+            listItem.classList.add("list-item");
+            listItem.innerHTML = `
+                <a class="hover-eff" href="${items[j].url}">
+                    <span class="text">${items[j].title}</span>
+                </a>
+            `
+            list.appendChild(listItem);
+        }
+
+        container.appendChild(list);
+        newSec.appendChild(container);
+        mainSec.appendChild(newSec);
     }
 }
 
